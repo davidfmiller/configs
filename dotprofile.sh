84bae48590191ede4668818de5c5036aacbc4298
David Miller <davidfmiller@me.com>
2013-11-22 09:37:00 -0800

    add python alias

--- dotprofile.sh
+++ dotprofile.sh
@@ -1,11 +1,14 @@
 
 function __l () {  ls -l "$@" | awk '{$1=""; $2=""; $3=""; $4=""; $5=""; $6=""; $7=""; $8=""; sub("  ", " "); print}' | awk '{$1=$1}1' | grep -v "^$"; }
 function __bb () { bbedit "$@"; }
+function __py () { python "$@"; }
+
 
 alias l='__l'
 alias '~'="cd ~;l"
 alias '..'="cd ..;l"
 alias ll="ls -al"
-alias 'bb'='__bb'
+alias bb='__bb'
+alias py='__py'
 
 export PS1="\n\h: \w\n⚡  "

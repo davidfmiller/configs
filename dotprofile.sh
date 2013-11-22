c13a036340e2fe440801a370894db997ade409e0
David Miller <davidfmiller@me.com>
2013-11-21 16:27:53 -0800

    add gitignore

--- dotprofile.sh
+++ dotprofile.sh
@@ -1,6 +1,11 @@
 
-alias '~'="cd ~"
-alias '..'="cd .."
-alias l="ls -l | awk '{print \$NF}' | sed -e '1d'"
+function __l () {  ls -l "$@"  | awk '{print $NF}' | sed -e '1d'; }
+function __bb () { bbedit "$@"; }
+
+alias l='__l'
+alias '~'="cd ~;l"
+alias '..'="cd ..;l"
 alias ll="ls -al"
+alias 'bb'='__bb'
+
 export PS1="\n\h: \w\n⚡  "

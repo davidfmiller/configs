b82d8130e5c280c86c727e39eb65e9fb5e56b241
David Miller <davidfmiller@me.com>
2013-11-22 10:11:01 -0800

    ll now similar to l

--- dotprofile.sh
+++ dotprofile.sh
@@ -1,13 +1,14 @@
 
-function __l () {  ls -l "$@" | awk '{$1=""; $2=""; $3=""; $4=""; $5=""; $6=""; $7=""; $8=""; sub("  ", " "); print}' | awk '{$1=$1}1' | grep -v "^$"; }
+function __l ()  {  ls -l "$@" | awk '{$1=""; $2=""; $3=""; $4=""; $5=""; $6=""; $7=""; $8=""; sub("  ", " "); print}' | awk '{$1=$1}1' | grep -v "^$"; }
+function __ll () {  ls -al "$@" | awk '{$1=""; $2=""; $3=""; $4=""; $5=""; $6=""; $7=""; $8=""; sub("  ", " "); print}' | awk '{$1=$1}1' | grep -v "^$"; }
 function __bb () { bbedit "$@"; }
 function __py () { python "$@"; }
 
 
 alias l='__l'
+alias ll="__ll"
 alias '~'="cd ~;l"
 alias '..'="cd ..;l"
-alias ll="ls -al"
 alias bb='__bb'
 alias py='__py'
 

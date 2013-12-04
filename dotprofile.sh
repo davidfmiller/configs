22ea22646250a8ce6054bcd34d9692504ba39169
David Miller <davidfmiller@me.com>
2013-12-03 21:46:08 -0800

    re-org

--- dotprofile.sh
+++ dotprofile.sh
@@ -1,37 +1,30 @@
 
-# https://github.com/davidfmiller/lsemoji
-function __emoji () { python ~/Documents/git/lsemoji/lsemoji.py "$@"; }
-
-# https://github.com/davidfmiller/apples
-function __launchbar () { python ~/Documents/git/apples/launchbar "$@"; }
-
-function __bb () { bbedit "$@"; }
-function __py () { python "$@"; }
-function __cd () { cd "$@"; __emoji; }
-
 # ls
 function __l ()  { ls -l "$@" | awk '{$1=""; $2=""; $3=""; $4=""; $5=""; $6=""; $7=""; $8=""; sub("  ", " "); print}' | awk '{$1=$1}1' | grep -v "^$"; }
 function __ll () { ls -al "$@" | awk '{$1=""; $2=""; $3=""; $4=""; $5=""; $6=""; $7=""; $8=""; sub("  ", " "); print}' | awk '{$1=$1}1' | grep -v "^$"; }
+function __emoji () { python ~/Documents/git/lsemoji/lsemoji.py "$@"; } # https://github.com/davidfmiller/lsemoji
 
 alias l='__l'
 alias ll="__ll"
 alias lm="__emoji"
 
-
-alias lb="__launchbar"
-
+# sys commands
 alias r='rm'
 alias g='git'
 
-# cd
+# directory nav
+function __cd () { cd "$@"; __emoji; }
 alias 'c'="__cd"
 alias '..'="c .."
 alias '~'="c ~"
 
-
+# applications
+function __bb () { bbedit "$@"; }
+function __py () { python "$@"; }
+function __launchbar () { python ~/Documents/git/apples/launchbar "$@"; } # https://github.com/davidfmiller/apples
 alias bb='__bb'
 alias py='__py'
+alias lb="__launchbar"
 
-
-#PROMPT_COMMAND='echo $UU'
-export PS1="\n\u@\h: \w\n⚡  "
+# prompt
+export PS1="\n\u@\h: \w\n⚡  " #PROMPT_COMMAND='echo $UU'
